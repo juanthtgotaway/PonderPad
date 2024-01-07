1e9e3fd517e9472a5d30eda5c96f5d2376b0c22b
Juan Acevedo <jmamartinez18@gmail.com>
2024-01-07 16:23:13 -0700

    added get routes for the landing page and the notes page

--- server.js
+++ server.js
@@ -8,7 +8,17 @@ const PORT  = process.env.PORT || 3001;
 //This imports the middleware 
 app.use = (clog);
 //Allows for parsing json and URLencoded form data
-app.use(express.kson());
+app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use('/api', api);
 app.use(express.static('public'));
+
+//Get route for the landing page
+app.get('*', (req, res) =>
+    res.sendFile(path.join(__dirname, './public/index.html'))
+);
+
+//get route for the notes page
+app.get('/notes', (req, res) =>
+    res.sendFile(path.join(__dirname, './public/notes.html'))
+);
